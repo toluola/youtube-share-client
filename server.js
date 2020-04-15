1308335488a6cbfcea2697f55297c73c988e291e
toluola <toluola7@gmail.com>
2020-04-15 00:49:17 -0700

    fix heroku isssue

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = (process.env.PORT) || 5000;
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 const server = app.listen(port, () => {
