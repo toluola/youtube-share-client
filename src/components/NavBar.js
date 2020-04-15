07da0d96a6a5bb79d387facef9794ef440e15c14
toluola <toluola7@gmail.com>
2020-04-15 00:15:21 -0700

    fix heroku issues

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { ButtonContainer } from "../utils/Button";
 import { AuthConsumer } from "../../src/context";
 
